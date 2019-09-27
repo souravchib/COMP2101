50c5e01214f955a1264ef452a0bf15ad17924f85
souravchib <Souravchib263@gmail.com>
2019-09-26 21:23:59 -0700

    ddoneone

---
+++ bash/findsetidfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# this script generates a report of the files on the system that have the setuid permission bit turned on
+# it is for the lab1 exercise
+
+# Task 1 for the exercise is to modify it to also find and display the setgid files in a second listing
+# The second listing should display after the setuid file list, and be formatted similar to the setuid file list
+
+# Task 2 for the exercise is to modify it to also display the 10 largest files in the system, sorted by their sizes
+# The listing should include the file name, owner, and size in MBytes and be displayed after the listings of setuid and setgid files
+echo "Setuid files:"
+echo "============="
+find / -type f -executable -perm -4000 -ls 2>/dev/null |
+    sort -k 3
+echo "============="
+echo "Task 1 Find Setgid files:"
+echo "============="
+find / -type f -executable -perm -2000 -ls 2>/dev/null |
+    sort -k 3
+echo "============="
+echo "Task 2 for the exercise is to modify it to also display the 10 largest files in the system, sorted by their sizes"
+echo "============="
+find /home/ -type f -exec ls -l 2>/dev/null --block-size=M {} \; |
+  sort -rh -k5 |
+    head -n 10 |
+      gawk '{print $5, $3, $9}'
